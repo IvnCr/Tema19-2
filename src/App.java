2eed9c164bd8b79b6a6b1853f46c2556baa02618
IvnCr <icr0015@alu.medac.es>
2024-05-05 11:45:03 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int dato1 = 30;
+        int dato2 = 20;
+
+        int total = 0;
+        int total2 = 0;
+        int total3 = 0;
+
+        total = dato1 + dato2;
+        total2 = dato1 - dato2;
+        total3 = dato1 * dato2;
+
+        System.out.println("Total de la suma: " + total);
+        System.out.println("Total de la resta: " + total2);
+        System.out.println("Total de la multiplacion: " + total3);
+    }
+}
